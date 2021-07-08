0cbf7b7a60e31427337fc5ea8b2ccc9925a506f5
Benedikt Schnatterebeck <benediktms@hey.com>
2021-07-08 20:55:00 +0100

    qCleanup

--- app/interactors/users/login_interactor.rb
+++ app/interactors/users/login_interactor.rb
@@ -5,21 +5,21 @@ module Users
     delegate :email, :password, to: :context
 
     def call
-      # return nil if password.empty?
       return unless password
 
       user = User.find_by(email: email)
-      return context.fail! error: 'Invalid credentials. Please try again' unless user
+      return context.fail! error: "Invalid credentials. Please try again" unless user
 
       if user.authenticate(password)
         user.token = user.generate_token
         user.save
+        binding.pry
 
-        context.message = 'Successfully logged in'
+        context.message = "Successfully logged in"
         context.user = user
         context.token = user.token
       else
-        context.fail! error: 'Login failed'
+        context.fail! error: "Login failed"
       end
     end
   end
